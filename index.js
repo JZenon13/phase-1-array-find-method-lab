f38b54d68a7aed244e1c1b93919cfe5190ced306
jesse zenon <jesse.zenon@gmail.com>
2022-04-06 23:01:13 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+// const record = [
+//   { year: "2015", result: "L" },
+//   { year: "2014", result: "N/A" },
+//   { year: "2013", result: "W" },
+// ];
+
+// const superbowlWin = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i].result === "W") {
+//       return array[i].year;
+//     }
+//   }
+// };
+
+const superbowlWin = (array) => {
+  let sbWin = array.find((object) => object.result === "W");
+  if (sbWin === undefined) {
+    return sbWin;
+  } else {
+    return sbWin.year;
+  }
+};
+superbowlWin();
